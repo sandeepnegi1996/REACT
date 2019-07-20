cc274f55173159b6fe7d2ec273b4d2e7ba0eea53
sandeep singh negi <9041128179sandy@gmail.com>
2019-07-20 12:35:04 +0530

    functional component done

---
+++ Day2_Components/learncomponent/src/components/Greet.js
@@ -0,0 +1,13 @@
+
+import React from 'react';
+
+
+// function Greet2() {
+
+//     return <h1>Hello Sandeep this is functional component without ES6</h1>
+    
+// }
+
+const Greet=()=> <h1>This is functional component using arrow function</h1>
+
+export default Greet;
